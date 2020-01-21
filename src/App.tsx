c8d0f3f9357886754c0227cb834786bcf456d0fd
parut.singhapun <parut.singhapun@accenture.com>
2020-01-21 10:48:48 +0700

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import './App.css';
 import Dropdown from 'react-dropdown'
 import 'react-dropdown/style.css'
+import { Route } from 'react-router-dom'
 
 import NormalList from './components/NormalList'
 import ReactWindowList from './components/ReactWindowList'
-
+import Home from './components/Home'
 
 function generateOption(): any {
   let options = []
-  for (let i = 0; i < 100000; i++) {
-    options.push('Options ', i + 1)
+  for (let i = 0; i < 1000000; i++) {
+    options.push('Options ' + (i + 1))
   }
   return options
 }
@@ -18,20 +19,10 @@ function generateOption(): any {
 const App: React.FC = () => {
   return (
     <div className="App">
-      <div id="simple-dd" style={{ height: "200px", width: "100%", overflow: "scroll" }}>
-        {/* <NormalList options={generateOption()} /> */}
-      </div>
-      <br />
-      <br />
-      <br />
-      <div>dodo</div>
-      <br />
-      <br />
-      <br />
-      <div id="react-dd" style={{ height: 1000, width: "100%" }}>
-        <ReactWindowList  options={generateOption()} />
-      </div>
-    </div>
+      <Route path="/" component={Home} />
+      <Route path="/normal" component={NormalList} />
+      <Route path="/react-window" component={Home} />
+</div>
   );
 }
 
---
+++ src/components/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { homedir } from 'os'
+
+const Home = () => {
+
+  return (
+    <div>
+      <button> <a href="/normal"> Normal list</a></button>
+      <button> <a href="/react-window"> React window list</a></button>
+    </div>
+  )
+}
+
+export default Home
